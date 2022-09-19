df10a07e67791d4c8b01849363e3d31f0d4237c4
Hunter Breathat <nexushunterdev@gmail.com>
2022-09-19 13:05:26 -0400

    Alias: More tweaks

--- lib/src/api/alias/client.dart
+++ lib/src/api/alias/client.dart
@@ -4,14 +4,19 @@ import 'package:opensearch_dart/src/api/common/responses.dart';
 
 import '../common/api_client.dart';
 
+/// An alias is a virtual pointer that you can use to reference one or more indices.
+///
+/// Creating and updating aliases are atomic operations, so you can reindex your
+/// data and point an alias at it without any downtime.
 class AliasClient extends ApiClient {
   AliasClient({required super.client, super.signer});
 
   FutureOr<AcknowledgeResponse> _makeRequest({
     required Map<String, dynamic> actions,
     String? filter,
-    bool isHidden = false,
-    bool mustExist = true,
+    bool? isHidden,
+    bool? mustExist,
+    bool? isWriteIndex,
     String? routing,
     String? indexRouting,
     String? searchRouting,
@@ -19,13 +24,29 @@ class AliasClient extends ApiClient {
     Duration timeout = const Duration(seconds: 30),
   }) async {
     return await client
-        .post('_aliases', data: <String, List<Map<String, dynamic>>>{
-          'actions': [
-            if (actions.containsKey('add')) {'add': actions['add']},
-            if (actions.containsKey('remove')) {'remove': actions['remove']},
-            // for (var action in actions.entries) {action.key: action.value},
-          ],
-        })
+        .post(
+          '_aliases',
+          // TODO: Many of these settings aren't valid though the documentation
+          //  states otherwise
+          // https://opensearch.org/docs/latest/opensearch/rest-api/alias/#request-body
+          data: <String, dynamic>{
+            if (filter != null) 'filter': filter,
+            if (isHidden != null) 'is_hidden': isHidden,
+            if (mustExist != null) 'must_exist': mustExist,
+            if (isWriteIndex != null) 'is_write_index': isWriteIndex,
+            if (routing != null) 'routing': routing,
+            if (searchRouting != null) 'search_routing': searchRouting,
+            'actions': [
+              if (actions.containsKey('add')) {'add': actions['add']},
+              if (actions.containsKey('remove')) {'remove': actions['remove']},
+              // TODO: Support for removeIndex required.
+            ],
+          },
+          queryParameters: <String, String>{
+            'master_timeout': '${masterTimeout.inSeconds}s',
+            'timeout': '${timeout.inSeconds}s',
+          },
+        )
         .onError(onErrorResponse(endpoint: 'alias.aliases'))
         .then(
           (value) => AcknowledgeResponse(acknowledged: value.statusCode == 200),
@@ -36,8 +57,9 @@ class AliasClient extends ApiClient {
     required String alias,
     required String index,
     String? filter,
-    bool isHidden = false,
-    bool mustExist = true,
+    bool? isHidden,
+    bool? mustExist,
+    bool? isWriteIndex,
     String? routing,
     String? indexRouting,
     String? searchRouting,
@@ -61,6 +83,7 @@ class AliasClient extends ApiClient {
       indexRouting: indexRouting,
       masterTimeout: masterTimeout,
       timeout: timeout,
+      isWriteIndex: isWriteIndex,
     );
   }
 
@@ -69,8 +92,9 @@ class AliasClient extends ApiClient {
     required String index,
     required String alias,
     String? filter,
-    bool isHidden = false,
-    bool mustExist = true,
+    bool? isHidden,
+    bool? mustExist,
+    bool? isWriteIndex,
     String? routing,
     String? indexRouting,
     String? searchRouting,
@@ -94,6 +118,7 @@ class AliasClient extends ApiClient {
       indexRouting: indexRouting,
       masterTimeout: masterTimeout,
       timeout: timeout,
+      isWriteIndex: isWriteIndex,
     );
   }
 
@@ -103,14 +128,6 @@ class AliasClient extends ApiClient {
       actions: <String, String>{
         'remove': index,
       },
-      // filter: filter,
-      // isHidden: isHidden,
-      // mustExist: mustExist,
-      // routing: routing,
-      // searchRouting: searchRouting,
-      // indexRouting: indexRouting,
-      // masterTimeout: masterTimeout,
-      // timeout: timeout,
     );
   }
 }
